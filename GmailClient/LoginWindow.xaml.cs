d8376f2753d97efd1c86c3dedd779689e80db012
Denys <dspeakers8@gmail.com>
2022-07-10 23:57:06 +0300

    Update using block.

--- GmailClient/LoginWindow.xaml.cs
+++ GmailClient/LoginWindow.xaml.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using MailKit;
 using MailKit.Net.Imap;
-using MailKit.Net.Smtp;
-using MailKit.Search;
 using MailKit.Security;
-using MimeKit;
 
 namespace GmailClient
 {
